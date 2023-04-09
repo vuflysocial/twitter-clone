0d3fb17d9ae72759f2670d6bb7213a5fa9950144
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-09 06:24:47 -0700

    Update Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,16 +1,11 @@
 import { signOut } from 'next-auth/react';
-import { BiLogOut } from 'react-icons/bi';
-import { BsHouseFill, BsBellFill } from 'react-icons/bs';
-import { FaUser } from 'react-icons/fa';
-
 import useCurrentUser from '@/hooks/useCurrentUser';
-
 import SidebarItem from './SidebarItem';
-import placeholderLogo from './images/placeholder.png';
+import placeholder from './images/placeholder.png';
 import SidebarClawButton from './SidebarClawButton';
 
 type SidebarItem = {
-  icon: React.ElementType;
+  icon: string; // Change type to string to accommodate image sources
   label: string;
   href: string;
   auth?: boolean;
@@ -22,19 +17,19 @@ const Sidebar = (): JSX.Element => {
 
   const items: SidebarItem[] = [
     {
-      icon: BsHouseFill,
+      icon: '/path/to/home-icon.png', // Replace with actual image source
       label: 'Home',
       href: '/',
     },
     {
-      icon: BsBellFill,
+      icon: '/path/to/notifications-icon.png', // Replace with actual image source
       label: 'Notifications',
       href: '/notifications',
       auth: true,
       alert: currentUser?.hasNotification,
     },
     {
-      icon: FaUser,
+      icon: '/path/to/profile-icon.png', // Replace with actual image source
       label: 'Profile',
       href: `/users/${currentUser?.id}`,
       auth: true,
@@ -46,21 +41,20 @@ const Sidebar = (): JSX.Element => {
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
         <div>
-        <img src={placeholderLogo.src} alt="Placeholder logo" width={40} height={40} style={{marginTop: '10px'}} />
-
-    </div>
+        <img src={placeholder.src} alt="Placeholder logo" width={40} height={40} style={{marginTop: '10px'}} />
+        </div>
           {items.map((item: SidebarItem) => (
             <SidebarItem
               key={item.href}
               alert={item.alert}
               auth={item.auth}
               href={item.href}
-              
+              icon={<img src={item.icon} alt={item.label} width={20} height={20} />} // Render image element
               label={item.label}
             />
           ))}
           {currentUser && (
-            <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />
+            <SidebarItem onClick={() => signOut()} icon={<img src="/path/to/logout-icon.png" alt="Logout" width={20} height={20} />} label="Logout" /> // Replace with actual image source
           )}
           <SidebarClawButton />
         </div>
